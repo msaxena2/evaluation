54c9bac7e0d416df77275244c2b19e796896e585
Manasvi Saxena <manasvi.saxena@runtimeverification.com>
2016-01-07 13:24:10 -0600

    initial commit

---
+++ Tools/valgrind.py
@@ -0,0 +1,20 @@
+from benchmark import Tool
+
+
+class Valgrind(Tool):
+
+    def run(self):
+
+
+    def init(self):
+        #valgrind needs to compile with gcc
+            
+
+    def __init__(self):
+        Tool.__init__(self)
+
+    def analyze(self):
+        Tool.analyze(self)
+
+    def cleanup(self):
+        Tool.cleanup(self)
---
+++ benchmark.py
@@ -0,0 +1,21 @@
+__author__ = 'manasvi'
+
+
+class Tool:
+
+    def __init__(self, benchmark_location):
+        self.input_file = benchmark_location
+
+    def init(self):
+        pass
+
+    def run(self):
+        pass
+
+    def analyze(self):
+        pass
+
+    def cleanup(self):
+        pass
+
+
