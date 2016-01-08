03f05c661816c2a37438a9f3fa1bba42c7a1b203
Manasvi Saxena <manasvi.saxena@runtimeverification.com>
2016-01-08 13:26:55 -0600

    Added some more files + valgrind init working

--- Tools/valgrind.py
+++ Tools/valgrind.py
@@ -1,17 +1,24 @@
-from benchmark import Tool
-
+from tool import Tool
+from utils import Info
+import os
+import subprocess
 
 class Valgrind(Tool):
 
     def run(self):
-
+        pass
 
     def init(self):
-        #valgrind needs to compile with gcc
-            
+        os.chdir(os.path.expanduser(self.benchmark_path))
+        subprocess.check_call(["autoreconf", "--install"])
+        subprocess.check_call(["autoconf"])
+        subprocess.check_call(["automake"])
+        subprocess.check_call(["./configure"])
+        subprocess.check_call(["make"])
 
-    def __init__(self):
-        Tool.__init__(self)
+    def __init__(self, benchmark_path, info_csv):
+        self.info = Info(info_csv)
+        self.benchmark_path = benchmark_path
 
     def analyze(self):
         Tool.analyze(self)
--- benchmark.py
+++ benchmark.py
@@ -1,23 +1,8 @@
 __author__ = 'manasvi'
-from utils import Info
-class Tool:
+from Tools.valgrind import Valgrind
 
-    def __init__(self, benchmark_location):
-        self.input_file = benchmark_location
-
-    def init(self):
-        pass
-
-    def run(self):
-        pass
-
-    def analyze(self):
-        pass
-
-    def cleanup(self):
-        pass
 
 if __name__ == '__main__':
-    info = Info("utils.csv")
-    info.get_spec_dict()
+    valgrind = Valgrind("/Users/manasvi/Documents/itc-benchmarks/", "utils.csv")
+    valgrind.init()
 
