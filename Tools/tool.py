fcb689e4b05716b8645212a6469c151c36fa056c
Manasvi Saxena <manasvi.saxena@runtimeverification.com>
2016-01-08 14:48:28 -0600

    renamed

---
+++ Tools/tool.py
@@ -0,0 +1,16 @@
+__author__ = 'manasvi'
+class Tool:
+
+    def __init__(self):
+        pass
+    def init(self):
+        pass
+
+    def run(self):
+        pass
+
+    def analyze(self):
+        pass
+
+    def cleanup(self):
+        pass
