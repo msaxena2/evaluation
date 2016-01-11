bedf3a913535a2efcfa5dc5e24510696eda8900b
Manasvi Saxena <manasvi.saxena@runtimeverification.com>
2016-01-10 23:46:10 -0600

    small changes

--- Tools/valgrind.py
+++ Tools/valgrind.py
@@ -8,17 +8,30 @@ class Valgrind(Tool):
     def run(self, verbose=False, log_location=None):
         output_dict = {}
         spec_dict = self.info.get_spec_dict()
-        for i in xrange(1, len(spec_dict.keys()) + 1):
+        for i in xrange(1, 3):#len(spec_dict.keys()) + 1):
             output_dict[i] = {"count": spec_dict[i]["count"], "TP": 0, "FN": 0}
             for j in range(i, spec_dict[i]["count"]):
                 arg = str('%03d' % i) + str('%03d' % j)
-                output = subprocess.check_call(["valgrind", "./01.w_Defects/01_w_Defects", arg])
-                if output != 0:
+                try:
+                    output_w = subprocess.check_call(["valgrind", "./01.w_Defects/01_w_Defects", arg])
+                except subprocess.CalledProcessError:
                     output_dict[i]["TP"] += 1
-                output = subprocess.check_call(["valgrind", "./02.wo_Defects/02_wo_Defects", arg])
-                if output == 0:
+                try:
+                    output_wo = subprocess.check_call(["valgrind", "./02.wo_Defects/02_wo_Defects", arg])
+                    # Update Data record
+                except subprocess.CalledProcessError:
                     output_dict[i]["FN"] += 1
-        print spec_dict
+                if verbose:
+                    whole_path = os.path.expanduser(log_location)
+                    mode = 'a'
+                    if not os.path.exists(whole_path) or (i == 1 and j == 0):
+                        mode = 'w+'
+                    with open(whole_path, mode) as output_file:
+                            output_file.write(output_w)
+                            output_file.write(output_wo)
+        print output_dict
+
+
 
     def init(self):
         os.chdir(os.path.expanduser(self.benchmark_path))
