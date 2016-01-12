35cf40ca818bb292b76f8894f11407b75f7c97ce
Manasvi Saxena <manasvi.saxena@runtimeverification.com>
2016-01-12 14:36:51 -0600

    changes to the scripts

--- benchmark.py
+++ benchmark.py
@@ -1,11 +1,14 @@
 __author__ = 'manasvi'
 from Tools.valgrind import Valgrind
 from Tools.valgrind_rv import ValgrindRV
-
+import sys
 
 if __name__ == '__main__':
     #valgrind = Valgrind("~/Documents/itc-benchmarks/", "utils.csv")
     #valgrind.init()
     #valgrind.run()
-    valgrind = ValgrindRV("/Users/manasvi/Documents/c-semantics/examples/error-codes")
-    valgrind.run()
+    if len(sys.argv) < 2:
+        print "Needed: Path to Benchmark as arguments"
+    else:
+        valgrind = ValgrindRV(sys.argv[1])
+        valgrind.analyze()
