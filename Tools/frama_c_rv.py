b66862eeff2262a2d47aa1e500d9ee054350092f
Manasvi Saxena <manasvi.saxena@runtimeverification.com>
2016-01-15 13:03:09 -0600

    working on logic for running the tests

--- Tools/frama_c_rv.py
+++ Tools/frama_c_rv.py
@@ -5,11 +5,12 @@ import subprocess32 as subprocess
 import signal
 from tabulate import tabulate
 
+
 class TimeoutException(Exception):
     pass
 
-class FramaCRV(Tool):
 
+class FramaCRV(Tool):
     def signal_handler(self, signum, frame):
         raise TimeoutException("Timed out!")
 
@@ -18,17 +19,17 @@ class FramaCRV(Tool):
         error_code_dict = {}
         total = 0
         os.chdir(self.benchmark_path)
-        for dir in filter(lambda x : os.path.isdir(x), os.listdir(os.getcwd())):
+        for dir in filter(lambda x: os.path.isdir(x), os.listdir(os.getcwd())):
             os.chdir(dir)
             print "In Directory: " + os.getcwd()
             file_list = os.listdir(os.getcwd())
-            for c_file in filter(lambda y : y.endswith(".c"), file_list):
+            for c_file in filter(lambda y: y.endswith(".c"), file_list):
                 c_files = []
                 exec_name = c_file.split('.')[0]
                 if "link" in c_file:
                     if "link1" not in c_file:
                         continue
-                    exec_name = exec_name.replace("-link1", "",)
+                    exec_name = exec_name.replace("-link1", "", )
                     c_files.append(c_file)
                     i = 2
                     while True:
@@ -56,7 +57,7 @@ class FramaCRV(Tool):
                 signal.alarm(5)
                 try:
                     command = ["frama-c", "-val"] + c_files
-                    #print command
+                    # print command
                     output = subprocess.check_output(command, stderr=subprocess.STDOUT)
                     if "warning" in output and c_file in output:
                         print output
@@ -74,31 +75,30 @@ class FramaCRV(Tool):
                 finally:
                     signal.alarm(0)
             os.chdir(self.benchmark_path)
-        return output_dict, error_code_dict, total
 
+        self.numbers_dict = output_dict
+        self.errors_dict = error_code_dict
+        self.total = total
 
-    def framac_data(self, output_dict, total):
+    def get_numbers(self):
         return {
-            "TP": str(float(output_dict["TP"])/(total/2) * 100),
-            "FP": str(float(output_dict["FP"])/(total/2) * 100)
+            "TP": str(float(self.numbers_dict["TP"]) / (self.total / 2) * 100),
+            "FP": str(float(self.numbers_dict["FP"]) / (self.total / 2) * 100)
         }
 
-    def init(self):
-        pass
+    def get_tool_name(self):
+        return "frama-c"
 
-    def tabulate(self, error_code_dict):
-        table = []
-        for key in error_code_dict.keys():
-            table.append([key, error_code_dict[key]["TP"], error_code_dict[key]["FP"]])
-        print tabulate(table, headers=["Error-Code", "TP", "FP"], tablefmt="fancy_grid")
-
+    def get_errors(self):
+        return self.errors_dict
 
     def __init__(self, benchmark_path):
         self.benchmark_path = os.path.expanduser(benchmark_path)
+        self.numbers_dict = {}
+        self.errors_dict = {}
+        self.total = None
 
     def analyze(self):
-        output_dict, error_dict, total = self.run()
-        return output_dict, self.framac_data(output_dict, total)
 
     def cleanup(self):
         Tool.cleanup(self)
--- benchmark.py
+++ benchmark.py
@@ -4,22 +4,36 @@ from Tools.valgrind_rv import ValgrindRV
 from Tools.compcert_rv import CompcertRV
 from Tools.ub_san_rv import UBSanRV
 from Tools.frama_c_rv import FramaCRV
+from tabulate import tabulate
 import sys
 
 if __name__ == '__main__':
-    #valgrind = Valgrind("~/Documents/itc-benchmarks/", "utils.csv")
-    #valgrind.init()
-    #valgrind.run()
+    # valgrind = Valgrind("~/Documents/itc-benchmarks/", "utils.csv")
+    # valgrind.init()
+    # valgrind.run()
     if len(sys.argv) < 2:
         print "Needed: Path to Benchmark as arguments"
     else:
-      #valgrind = ValgrindRV(sys.argv[1])
-      #valgrind.analyze()
-        #compcert = CompcertRV(sys.argv[1])
-        #compcert.analyze()
-        #ubsan = UBSanRV(sys.argv[1])
-        #ubsan.analyze()
+        # valgrind = ValgrindRV(sys.argv[1])
+        # valgrind.analyze()
+        # compcert = CompcertRV(sys.argv[1])
+        # compcert.analyze()
+        # ubsan = UBSanRV(sys.argv[1])
+        # ubsan.analyze()
+        path = sys.argv[1]
+        tools = [ValgrindRV(path)]
+        # run the tools
+        tools = map(lambda x: x.run(), tools)
+        numbers = []
+        map(lambda x: numbers.append([x.get_tool_name, x.get_numbers["TP"], x.get_numbers["FP"]]), tools)
+        error_codes = reduce(lambda x, y: merge(x, y), map(lambda z: z.get_errors()), tools, {"TP": set("a"), "FP": set("b")})
+        print numbers
+        print error_codes
 
-        framac = FramaCRV(sys.argv[1])
-        framac.analyze()
 
+def merge(dict1, dict2):
+    retdict = {}
+    for key in dict1.keys():
+        retdict[key] = {"TP": set(dict1[key]["TP"].items(), dict2[key]["TP"].items()),
+                        "FP": set(dict1[key]["FP"].items(), dict2[key]["FP"].items())}
+    return retdict
