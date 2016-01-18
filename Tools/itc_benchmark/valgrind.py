23c22589db444b5b9d977e8f68153adf41792374
Manasvi Saxena <manasvi.saxena@runtimeverification.com>
2016-01-18 10:21:00 -0600

    alarm related logic

--- Tools/itc_benchmark/valgrind.py
+++ Tools/itc_benchmark/valgrind.py
@@ -4,36 +4,51 @@ import subprocess
 from Tools.rv_benchmark.tool import Tool
 from utils.utils import Info
 import progressbar
+import signal
+
+class TimeoutException(Exception):
+    pass
 
 class Valgrind(Tool):
 
+    def signal_handler(self, signum, frame):
+        raise TimeoutException("Timed out!")
+
     def run(self, verbose=False, log_location=None):
         output_dict = {}
-        print "here"
         spec_dict = self.info.get_spec_dict()
         os.chdir(self.benchmark_path)
         for i in range(1, len(spec_dict.keys()) + 1):
-            output_dict[i] = {"count": spec_dict[i]["count"], "TP": 0, "FN": 0}
+            output_dict[i] = {"count": spec_dict[i]["count"], "TP": 0, "FP": 0}
             print self.name + " being tested on file " + str(i)
-            for j in range(i, spec_dict[i]["count"]):
+            bar = progressbar.ProgressBar()
+            for j in bar(range(1, spec_dict[i]["count"])):
                 arg = [str('%03d' % i) + str('%03d' % j)]
                 try:
-                    output_w = subprocess.check_output(["valgrind", "--error-exitcode=-1","./01.w_Defects/01_w_Defects"] + arg)
+                    signal.signal(signal.SIGALRM, self.signal_handler)
+                    signal.alarm(10)
+                    mode = "TP"
+                    output_w = subprocess.check_output(["valgrind", "--error-exitcode=-1", "./01.w_Defects/01_w_Defects"] + arg, stderr=subprocess.STDOUT)
+                    mode = "FP"
+                    output_wo = subprocess.check_output(["valgrind", "--error-exitcode=-1", "./02.wo_Defects/02_wo_Defects"] + arg, stderr=subprocess.STDOUT)
                 except subprocess.CalledProcessError:
-                    output_dict[i]["TP"] += 1
-                try:
-                    output_wo = subprocess.check_output(["valgrind", "--error-exitcode=-1","./02.wo_Defects/02_wo_Defects"] + arg)
-                    # Update Data record
-                except subprocess.CalledProcessError:
-                    output_dict[i]["FN"] += 1
+                    if mode == "TP":
+                        output_dict[i]["TP"] += 1
+                    else:
+                        output_dict[i]["FP"] += 1
+                except TimeoutException:
+                    continue
+                finally:
+                    #reset the alarm
+                    signal.alarm(0)
                 if verbose:
                     whole_path = os.path.expanduser(log_location)
                     mode = 'a'
                     if not os.path.exists(whole_path) or (i == 1 and j == 0):
                         mode = 'w+'
                     with open(whole_path, mode) as output_file:
-                            output_file.write(output_w)
-                            output_file.write(output_wo)
+                        output_file.write(output_w)
+                        output_file.write(output_wo)
         print output_dict
 
 
