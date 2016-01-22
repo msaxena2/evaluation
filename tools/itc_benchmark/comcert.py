88ed85264c196c717713179b213b96482f8a7a88
Manasvi Saxena <manasvi.saxena@runtimeverification.com>
2016-01-22 13:29:17 -0600

    improving compcert by removing cil related stuff

--- tools/itc_benchmark/comcert.py
+++ tools/itc_benchmark/comcert.py
@@ -16,12 +16,11 @@ class Compcert(Tool):
         raise TimeoutException("Timed out!")
 
     def get_compcert_command(self, cur_dir, file_prefix):
-        cil_file = os.path.join(self.benchmark_path, cur_dir, "compcert_temp", file_prefix + "-temp.cil.c")
+        cil_file = os.path.join(self.benchmark_path, cur_dir, "compcert_temp", file_prefix + "-temp.c")
         if not os.path.exists(cil_file):
             return []
 
-        utils.external_info.sanitize_cil_file(cil_file)
-        return ["ccomp", "-interp", "-fbitfields", "-fstruct-passing", cil_file]
+        return ["ccomp", "-interp", "-fbitfields", "-fstruct-passing", "-I../include/", cil_file]
 
     def run(self, verbose=False, log_location=None):
         relevant_dirs = ["02.wo_Defects"]
@@ -33,7 +32,7 @@ class Compcert(Tool):
         for cur_dir in relevant_dirs:
             spec_dict = self.info.get_spec_dict()
             mapping_dict = self.info.get_file_mapping()
-            for i in range(1, len(spec_dict.keys()) + 1):
+            for i in range(1, 3): #len(spec_dict.keys()) + 1):
                 if i not in output_dict:
                     output_dict[i] = {"count": spec_dict[i]["count"], "TP": 0, "FP": 0}
                 file_prefix = mapping_dict[i]
@@ -41,36 +40,23 @@ class Compcert(Tool):
                 # bar = progressbar.ProgressBar(redirect_stdout=True)
                 for j in range(1, spec_dict[i]["count"]):
                     vflag = str('%03d' % j)
-                    cilly_command = utils.external_info.get_cilly_commmand(self.benchmark_path, cur_dir, file_prefix,
-                                                                           "compcert_temp", vflag)
-                    if len(cilly_command) == 0:
-                        break
                     try:
-                        subprocess.check_output(cilly_command, stderr=subprocess.STDOUT)
-                    except subprocess.CalledProcessError:
-                        continue
-                    try:
+                        file_path = os.path.join(self.benchmark_path, cur_dir, file_prefix + ".c")
+                        temp_store_file_path = os.path.join(self.benchmark_path, cur_dir, "bootstrap_files", file_prefix + "-temp.c")
+                        utils.external_info.bootstrap_file(file_path, temp_store_file_path, j)
                         compcert_command = self.get_compcert_command(cur_dir, file_prefix)
                         if len(compcert_command) != 0:
                             signal.signal(signal.SIGALRM, self.signal_handler)
                             signal.alarm(10)
                             output = subprocess.check_output(compcert_command, stderr=subprocess.STDOUT)
-                            if log_location is not None:
-                                log_file.write("----- Folder " +  cur_dir + " file " + str(i) + "flag " + str(vflag) + "--- marked OK.")
-                                log_file.write(output)
                     except subprocess.CalledProcessError as e:
-                        signal.alarm(0)
-                        print e.output
-                        input = raw_input("Is this an actual error?: ")
-                        if input == "y":
-                            if "w_Defects" in cur_dir:
-                                output_dict[i]["TP"] += 1
-                            else:
-                                output_dict[i]["FP"] += 1
-                            log_file.write("----- Folder " +  cur_dir + " file " + str(i) + "flag " + str(vflag) + "--- marked Not OK.")
+
+                        if "w_Defects" in cur_dir:
+                            output_dict[i]["TP"] += 1
                         else:
-                            log_file.write("----- Folder " +  cur_dir + " file " + str(i) + "flag " + str(vflag) + "--- marked OK.")
-                            log_file.write(output)
+                            output_dict[i]["FP"] += 1
+
+
                     except TimeoutException:
                         continue
                     finally:
